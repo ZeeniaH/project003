29261c71dc01f238ed82a31b23e66bfb64e13eab
abdullahzubair55 <abdullahdosta@gmail.com>
2020-06-08 04:37:49 +0500

    create basic react app

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hello.css';
+
+
+
+function Hello({name}) {
+    return <p className = "myname">This is para in Hello tag updated {name} </p>
+
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div><App name = " Abdullah Zubair" age = "24"/></div>,
   document.getElementById('root')
 );
 
